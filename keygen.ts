e4c45788ea6f7aec998db87261ac908e2905cc63
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-07-26 02:45:17 +0300

    keypair

--- keygen.ts
+++ keygen.ts
@@ -1,1 +1,5 @@
 import { Keypair } from "@solana/web3.js";
+
+let kp = Keypair.generate()
+console.log(`You've generated a new Solana wallet: ${kp.publicKey.toBase58()}`);
+console.log(`Solana Wallet Secret Key: ${kp.secretKey}]`);
