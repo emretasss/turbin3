b378d8bdfa710033a9afecd312b9e1aba0fbe6fb
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-07-26 03:05:23 +0300

    sitftpyd

--- airdrop.ts
+++ airdrop.ts
@@ -5,3 +5,13 @@ const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection to devnet SOL tokens
 const connection = new Connection("https://api.devnet.solana.com");
+Finally, we're going to claim 2 devnet SOL tokens:
+(async () => {
+    try {        // We're going to claim 2 devnet SOL tokens
+        const txhash = await connection.requestAirdrop(keypair.publicKey, 2 * LAMPORTS_PER_SOL);
+        console.log(`Success! Check out your TX here: 
+        https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+    } catch(e) {
+        console.error(`Oops, something went wrong: ${e}`)
+    }
+})();
