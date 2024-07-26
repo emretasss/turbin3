b2a58dd147cd02cb507e423b744d254eadb209f5
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-07-26 03:04:50 +0300

    airdropts

--- airdrop.ts
+++ airdrop.ts
@@ -1,4 +1,7 @@
 import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js"
-
 import wallet from "./dev-wallet.json"
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
+
+//Create a Solana devnet connection to devnet SOL tokens
+const connection = new Connection("https://api.devnet.solana.com");
